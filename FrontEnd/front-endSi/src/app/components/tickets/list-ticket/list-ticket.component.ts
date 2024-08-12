0c8efdf41a169710f5199253d1d9672ee2a1de19
zineb zarda <zinebzarda1@gmail.com>
2024-08-13 00:56:19 +0100

    show teckit

--- FrontEnd/front-endSi/src/app/components/tickets/list-ticket/list-ticket.component.ts
+++ FrontEnd/front-endSi/src/app/components/tickets/list-ticket/list-ticket.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {TicketDeSupport} from "../../../models/ticket-de-support";
+import {TicketService} from "../../../services/ticket.service";
 
 @Component({
   selector: 'app-list-ticket',
   templateUrl: './list-ticket.component.html',
-  styleUrl: './list-ticket.component.css'
+  styleUrls: ['./list-ticket.component.css']
 })
-export class ListTicketComponent {
+export class ListTicketComponent implements OnInit {
+  tickets: TicketDeSupport[] = [];
 
+  constructor(private ticketService: TicketService) {}
+
+  ngOnInit(): void {
+    this.ticketService.getAllTickets().subscribe(
+      (data: TicketDeSupport[]) => {
+        this.tickets = data;
+      },
+      (error: any) => {
+        console.error('Erreur lors de la récupération des tickets:', error);
+      }
+    );
+  }
+
+  deleteTicket(id: number): void {
+    this.ticketService.deleteTicket(id).subscribe(
+      () => {
+        this.tickets = this.tickets.filter(ticket => ticket.id !== id);
+      },
+      (error: any) => {
+        console.error('Erreur lors de la suppression du ticket:', error);
+      }
+    );
+  }
 }
